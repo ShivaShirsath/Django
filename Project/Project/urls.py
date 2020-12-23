89dda680f45e601ce2856f954f924a585628b207
root <root@localhost>
2020-12-24 00:33:48 +0530

    committed

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Shiva's Website Admin Panel"
+admin.site.site_title = "Shiva's Admin Portal"
+admin.site.index_title = "Welcome to Shiva's Web Portal"
+	
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls'))
