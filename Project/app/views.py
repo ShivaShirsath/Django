6d50b7e7820b7524d350e4e4c40f068a2d6c9013
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2020-12-23 22:31:29 +0530

    committed

--- Project/app/views.py
+++ Project/app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1 align='center'>Home Page<h1>")
+    return render(request, 'templates/index.html')
+    #return HttpResponse("<h1 align='center'>Home Page<h1>")
 
 def about(request):
     return HttpResponse("<h1 align='center'>About<h1>")
