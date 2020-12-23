fb15fe945e98b73aa4ca0cbf77cabfdec96a6ff0
root <root@localhost>
2020-12-24 01:13:12 +0530

    committed

--- Project/app/urls.py
+++ Project/app/urls.py
@@ -4,6 +4,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('home', views.index, name='home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
 ]
