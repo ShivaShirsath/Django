82fcbaf98b8341c3eb8aca5c7e68477984708bb2
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2020-12-26 21:37:22 +0530

    committed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    firstname = models.TextField()
+    lastname = models.TextField()
+    email = models.TextField()
+    phone = models.TextField()
+    msg = models.TextField()
+    date = models.DateField()
