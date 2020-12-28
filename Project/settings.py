537ebd0cd9204999f0c3e33bb225454c535fc844
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2020-12-28 15:15:05 +0530

    committed

--- Project/settings.py
+++ Project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / "templates" ],
+        'DIRS': [ BASE_DIR / "docs" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
